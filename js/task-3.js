f95f74c2c9dda62aa36475a036f37f1202a26445
Vitalii <v.solonuha@gmail.com>
2020-08-23 15:22:38 +0300

    work 1-8

--- js/task-3.js
+++ js/task-3.js
@@ -12,3 +12,23 @@ const findLongestWord = function(string) {
 console.log(findLongestWord('Google do a roll')); // 'Google'
 
 console.log(findLongestWord('May the force be with you')); // 'force'*/
+function findLongestWord (string = "") {
+ let words = string.split(" ");
+ let longestWord = words[0];
+  for (const word of words) {
+    const isLongest = word.length > longestWord.length;
+    if (isLongest) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+console.log(findLongestWord('May the force be with you'));
+// 'force'
--- js/task-5.js
+++ js/task-5.js
@@ -14,3 +14,17 @@ console.log(checkForSpam('JavaScript weekly newsletter')); // false
 console.log(checkForSpam('Get best sale offers now!')); // true
 
 console.log(checkForSpam('[SPAM] How to earn fast money?')); // true*/
+
+function checkForSpam (str) { 
+  'use strict';
+  let message = str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale');
+  return message;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
